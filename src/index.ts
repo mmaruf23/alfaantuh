45118d4359398c1dc3a3e7f28a63951255261877
mmaruf <maruuf82@gmail.com>
2025-12-16 16:26:29 +0700

    Setting cors for new endpoint.
    Closes #15

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,8 @@ import { psm, store } from "./app/router";
 import { env } from "cloudflare:workers";
 import type { ApiResponse } from "./types";
 
-app.use(
-  "/psm/*",
-  cors({
-    origin: [env.ORIGIN],
-    allowMethods: ["GET"],
-  })
-);
+app.use("/psm/*", cors({ origin: [env.ORIGIN], allowMethods: ["GET"] }));
+app.use("/info/*", cors({ origin: [env.ORIGIN], allowMethods: ["GET"] }));
 
 app.route("psm", psm);
 app.route("info", store);
